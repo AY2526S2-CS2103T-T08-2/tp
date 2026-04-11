5f5a9766a0575871f5ebfcf2cb5555b0d1d921c2
yisheng <yishengtoh@outlook.com>
2026-04-12 03:00:42 +0800

    Fix Invalid Phone Number Regex

--- src/main/java/seedu/address/model/person/Phone.java
+++ src/main/java/seedu/address/model/person/Phone.java
@@ -11,8 +11,8 @@ public class Phone {
 
 
     public static final String MESSAGE_CONSTRAINTS =
-            "Phone numbers should only contain numbers, and it should be at least 3 digits long";
-    public static final String VALIDATION_REGEX = "\\d{3,}";
+            "Phone numbers should only be a valid Singapore nunber";
+    public static final String VALIDATION_REGEX = "^[689]\\d{7}$";
     public final String value;
 
     /**
